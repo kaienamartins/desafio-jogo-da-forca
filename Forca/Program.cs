69d20897537fea74fa0cc0e943f3ed3245bef532
Kaiena Martins <kaienafmartins@gmail.com>
2023-03-27 13:59:55 -0300

    final version

--- Forca/Program.cs
+++ Forca/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace jogoDaForca
 {
@@ -7,69 +8,69 @@ namespace jogoDaForca
         static void Main(string[] args)
         {
             string[] palavras = { "abacate", "amora", "banana", "brigadeiro", "caqui", "chuva", "daenerys", "damasco", "dinossauro", "elefante", "esquilo", "feijoada", "floripa", "gelatina", "gelo", "halloween", "hospital", "igreja", "iglu", "javali", "jamylle", "kaiena", "kiwi", "leia", "lua", "luke", "luz", "marreco", "martelo", "naboo", "navio", "nuvem", "ovelha", "ovo", "presunto", "prisma", "quasimodo", "queijo", "rei", "riacho", "sacripanta", "skywalker", "sol", "tatooine", "torresmo", "trem", "urso", "uva", "vendaval", "vinho", "waffle", "whovian", "xadrex", "xarope", "xerox", "yakisoba", "yogurt", "zebra", "zumbi" };
+            
             Random aleatorio = new Random();
-
+            //para fazer com que a escolha da palavra seja aleatória
             string palavraAleatoriaEscolhida = palavras[aleatorio.Next(palavras.Length)];
 
-            char[] palavraConvertida = palavraAleatoriaEscolhida.ToCharArray();
+            char[] palavraConvertida = palavraAleatoriaEscolhida.ToCharArray(); //tranforma a palavra em char
 
-            char[] palavraDoJogo = new char[palavraConvertida.Length];
+            char[] palavraDoJogo = new char[palavraConvertida.Length]; 
 
             int i = 0;
 
-            while (i < palavraConvertida.Length)
+            while (i < palavraConvertida.Length) //muda a palavra deixando ela tracejada e exibe no terminal
             {
                 palavraDoJogo[i] = '-';
                 Console.Write(palavraDoJogo[i]);
                 i++;
             }
 
-            bool acertou = false;
-            List<char> tentativas = new List<char>();
+            bool acertou = false; // pra controloar o while e só muda pra true quando acertar todas as palavras
+            List<char> tentativas = new List<char>(); // array pra armazenar as letras que foram usadas
 
             while (!acertou)
             {
                 Console.Write("\nDigite uma letra: ");
-                string paraUsarNoChar = Console.ReadLine();
+                string paraUsarNoChar = Console.ReadLine(); //variavel criada pra poder usar o char depois, pois dava erro de compilação
 
                 char letraDigitada = paraUsarNoChar[0];
     
 
                 if (!tentativas.Contains(letraDigitada))
                 {
-                    tentativas.Add(letraDigitada);
-                    bool letra = false;
+                    tentativas.Add(letraDigitada);   //colocar a letra dentro de tentativas
+                    bool letra = false; //pra verificar se a palavra tá contida na palavra aleatoria escolhida
 
                     for (int k = 0; k < palavraAleatoriaEscolhida.Length; k++)
                     {
-                        if (letraDigitada == palavraAleatoriaEscolhida[k])
+                        if (letraDigitada == palavraAleatoriaEscolhida[k]) // verifica se a letra que foi digitada está presente
                         {
-                            palavraDoJogo[k] = letraDigitada;
-                            letra = true;
+                            palavraDoJogo[k] = letraDigitada; // indice de palavra do jogo é igual a letra digitada
+                            letra = true; //letra contida, mudou pra true
                         }
                     }
 
-                    if (letra)
+                    if (letra) //se tiver a letra então o jogador recebe a mensagem
                     {
                         Console.WriteLine("Tem essa letra mesmo");
                     }
-                    else
+                    else //se não tiver a letra mensagem de erro
                     {
                         Console.WriteLine("Errou! (Faustão's voice). Não tem essa letra não");
                     }
-                    Console.WriteLine(palavraDoJogo);
-                    if (!palavraDoJogo.Contains('-'))
+                    Console.WriteLine(palavraDoJogo); // vai printando a palavra na tela
+                    if (!palavraDoJogo.Contains('-')) // se não tiver mais nenhum traço a palavra já foi completada
                     {
-                        acertou = true;
+                        acertou = true; //acertou todas as letras, mudou pra tru
                         Console.WriteLine($"Parabéns, você acertou a palavra! Era {palavraAleatoriaEscolhida}");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Você já tentou essa letra antes. Tente outra letra.");
+                    Console.WriteLine("Você já tentou essa letra antes. Tente outra letra."); //letra já digitada
                 }
             }
-            
         }
     } 
 }
